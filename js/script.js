58c8cc06288aa9dcdb87c3cf98cb3afa3ad7a213
anatoliy <anatolijdegtyar@yandex.ru>
2021-08-23 10:29:08 +0300

    Updates

---
+++ js/script.js
@@ -0,0 +1,31 @@
+'use strict'
+
+// Меню
+
+let mainNav = document.querySelector('.main-nav');
+let buttonToggle = document.querySelector('.main-nav__button-toggle');
+let headerMenu = document.querySelector(".header");
+
+mainNav.classList.remove('main-nav--no-js');
+
+buttonToggle.addEventListener('click', function() {
+  if (mainNav.classList.contains('main-nav--closed')) {
+    mainNav.classList.remove('main-nav--closed');
+    mainNav.classList.add('main-nav--opened');
+    headerMenu.classList.add("header__fixed");
+  } else {
+    mainNav.classList.add('main-nav--closed');
+    mainNav.classList.remove('main-nav--opened');
+    headerMenu.classList.remove("header__fixed");
+  }
+});
+
+// Фиксированное меню
+
+let menuFixed = function() {
+  if (window.pageYOffset > 10) {
+    headerMenu.classList.add("header__fixed");
+  } else {
+    headerMenu.classList.remove("header__fixed");
+  }
+};
